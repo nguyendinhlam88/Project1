d3fe349b447600a2034c4661713a86b7f14ab6c8
lamyaya88 <datascience@lamyaya88s-MacBook-Air.local>
2020-10-24 17:55:44 +0700

    Update bwt.cpp

--- bwt.cpp
+++ bwt.cpp
@@ -1,105 +1,124 @@
 #include <iostream>
-#include <fstream>
 #include <algorithm>
 #include <string.h>
+#include <map>
 
 using namespace std;
 
-typedef struct{
+struct Rotation{
 	int index;
 	char* suffix;
-}Rotation;
+};
 
-bool compareRotation(Rotation tmp1,Rotation tmp2){
-	//strcmp không đáp ứng được sách bwt.
-	int i = 0;
-	while(i < 50){
-		if(tmp1.suffix[i] > tmp2.suffix[i]){
-			return false;
-		} else if(tmp1.suffix[i] < tmp2.suffix[i]){
-			return true;
-		}
-		i++;
-		
-	}
+bool cmpRotation(Rotation tmp1, Rotation tmp2){
+	int cmp = strcmp(tmp1.suffix, tmp2.suffix);
+	if(cmp >= 0) return false;
 	return true;
 }
 
-int* buildSuffixArray(char* text, int len){
-	
-	Rotation tmp[len];
-	//Intantiate a dynamic suffix_arr to return.
-	int* suffix_arr = new int[len] ;
-
-	for(int i = 0; i < len; i++){
-		tmp[i].index = i;
-		tmp[i].suffix = text + i;
+void rotate(char* &text){
+	int n = strlen(text);
+	char temp = text[0];
+	for(int i = 0; i < n; i++){
+		if(i == n-1) text[n-1] = temp;
+		else text[i] = text[i+1];
 	}
-
-	//Sort tmp by tmp[i].suffix
-	sort(tmp, tmp + len, compareRotation);
-
-	//Assign tmp[i].index for suffix_arr[i] to know the original rotation order  
-	for(int j = 0; j < len; j++){
-		suffix_arr[j] = tmp[j].index;
-	}
-
-	return suffix_arr;
 }
 
-char* BWT(char* text, int* suffix_arr, int len){
-	char* bwt = new char[len];
+int* computeSuffixArrayIndex(char* text, int n, int &k){
+	Rotation temp[n];
+	int* suffix_arr_index = new int[n];
 
-	//Because rotation by 1 character.
-	int i = 0;
-	for(int i = 0; i < len; i++){
-		int j = suffix_arr[i] - 1;
-		if(j < 0) j += len;
-		bwt[i] = text[j];
+	//Mảng hậu tố.
+	for(int i = 0; i < n; i++){
+		temp[i].index = i;
+		temp[i].suffix =  new char[n];
+		if(i == 0) strcpy(temp[i].suffix, text);
+		else {
+			//Mỗi lần quay 1 so với xâu quay lần trước.
+			rotate(text);
+			strcpy(temp[i].suffix, text);
+		}
 	}
 
+	//Sắp xếp mảng temp theo thứ tự AlphaBeta của phần tử suffix : không hiểu tại sao nó sort() sai 1 cái :))
+	sort(temp, temp + n, cmpRotation);
+
+	//Tính suffix_arr_index dựa trên danh sách đã sắp xếp.
+	for(int j = 0; j < n; j++){
+		suffix_arr_index[j] = temp[j].index;
+		if(temp[j].index == 0) k = j; //Hàng chứa xâu gốc theo index 0.
+		delete [](temp[j].suffix);
+	}
+
+	rotate(text); //Quay về xâu cũ
+
+
+	return suffix_arr_index;
+}
+
+char* getLastColumn(char* text, int* suffix_arr_index, int n){
+	char* bwt = new char[n];
+	int index;
+
+	for(int i = 0; i < n; i++){
+		index = suffix_arr_index[i];
+		if(index == 0) bwt[i] = text[n-1];
+		else bwt[i] = text[index - 1];
+	}
 	return bwt;
 }
 
+//Inverse ngược từ n -> 1.
+char* inverseBWT(char* bwt, int n, int k){
+	//Mảng lưu trữ xâu sau khi khôi phục.
+	char* Q = new char[n];
 
+	//Mảng lưu trữ vị trí đầu tiên của các ký tự theo thứ tự trong F.
+	map<char, int> dict;
+	int dem = 0;
+	//Mảng theo thứ tự từ điển.
+	char F[n];
+	strcpy(F, bwt);
+	sort(F, F + n);
+	//Số lần xuất hiện ký tự bwt[i] trước bwt[i].
+	int C[n];
+	dict[bwt[0]] = 0;
+
+	for(int i = 1; i < n; i++){
+		if(F[i] != F[i-1]){
+			dict[F[i]] = i;
+		}
+		
+	}
+
+	string s = "";
+
+	for(int i = 0; i < n; i++){
+		if(s.length() == 0) C[i] = 0;
+		else {
+			C[i] = count(s.begin(), s.end(), bwt[i]);
+		} 
+		s += bwt[i];
+	}
+
+	for(int j = n - 1; j >= 0; j--){
+		Q[j] = bwt[k];
+		k = C[k] + dict[bwt[k]];
+	}
+	return Q;
+}
 
 int main(){
-
-	//Chuyển đổi xuôi.
-	// Input : "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES"
-   	ifstream myfile;
-    myfile.open("test1.txt");
-
-    if(myfile.is_open()){
-    	int begin = myfile.tellg();
-    	myfile.seekg(0, ios::end);
-    	int end = myfile.tellg();
-    	int size = end - begin;
-    	myfile.seekg(0);
-    	char text[size];
-    	int i = 0;
-
-    	while(!myfile.eof()){
-    		string line;
-    		getline(myfile, line);
-    		int i = 0;
-    		while(i < line.length()){
-    			text[i] = line[i];
-    			i++;
-    		}
-    	}
-
-    	int len = sizeof(text)/sizeof(char);
-    	int* suffix_arr = buildSuffixArray(text, len);
-    	char* output = BWT(text, suffix_arr, len);
-
-    	cout << output;
-
-    } 
-    else cout << "Fail to read file" << endl;
-
-    myfile.close();
-    //Chuyển đổi ngược.
-
-    return 0;
+	char arr[] = "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES";
+	int n = strlen(arr);
+	int k;
+	int* suffix_arr_index = computeSuffixArrayIndex(arr, n, k);
+	cout << "K :" << k << endl;
+	char* bwt = getLastColumn(arr, suffix_arr_index, n);
+	cout << "BWT :" << bwt << endl; 
+	char* original = inverseBWT(bwt, n, k);
+	cout << "original :" << original<< endl;
+	delete []original;
+	return 0;
 }
